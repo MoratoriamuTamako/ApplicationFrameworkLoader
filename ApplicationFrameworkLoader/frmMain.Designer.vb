56672a4b9290a24c14f8c9890265e2f5ad5a8e00
shiji <shiji@SHIJIANGHUI>
2016-09-12 14:09:05 +0800

    添加项目文件。

---
+++ ApplicationFrameworkLoader/frmMain.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.文件ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.msg = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.TabControlList = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TreeViewMe = New System.Windows.Forms.TreeView()
+        Me.TreeViewAll = New System.Windows.Forms.TreeView()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TabControlDetail = New System.Windows.Forms.TabControl()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.TabPage4 = New System.Windows.Forms.TabPage()
+        Me.MenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.TabControlList.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.TabControlDetail.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.文件ToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(836, 25)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        '文件ToolStripMenuItem
+        '
+        Me.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem"
+        Me.文件ToolStripMenuItem.Size = New System.Drawing.Size(44, 21)
+        Me.文件ToolStripMenuItem.Text = "文件"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.msg})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 490)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(836, 22)
+        Me.StatusStrip1.TabIndex = 4
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'msg
+        '
+        Me.msg.Name = "msg"
+        Me.msg.Size = New System.Drawing.Size(134, 17)
+        Me.msg.Text = "ToolStripStatusLabel1"
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 25)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.TabControlList)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Panel1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(836, 465)
+        Me.SplitContainer1.SplitterDistance = 241
+        Me.SplitContainer1.TabIndex = 5
+        '
+        'TabControlList
+        '
+        Me.TabControlList.Alignment = System.Windows.Forms.TabAlignment.Bottom
+        Me.TabControlList.Controls.Add(Me.TabPage1)
+        Me.TabControlList.Controls.Add(Me.TabPage2)
+        Me.TabControlList.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControlList.Location = New System.Drawing.Point(0, 0)
+        Me.TabControlList.Multiline = True
+        Me.TabControlList.Name = "TabControlList"
+        Me.TabControlList.SelectedIndex = 0
+        Me.TabControlList.Size = New System.Drawing.Size(241, 465)
+        Me.TabControlList.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.TreeViewMe)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 4)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(233, 439)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "我的模块"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.TreeViewAll)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 4)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(233, 439)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "所有模块"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TreeViewMe
+        '
+        Me.TreeViewMe.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TreeViewMe.Location = New System.Drawing.Point(3, 3)
+        Me.TreeViewMe.Name = "TreeViewMe"
+        Me.TreeViewMe.Size = New System.Drawing.Size(227, 433)
+        Me.TreeViewMe.TabIndex = 0
+        '
+        'TreeViewAll
+        '
+        Me.TreeViewAll.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TreeViewAll.Location = New System.Drawing.Point(3, 3)
+        Me.TreeViewAll.Name = "TreeViewAll"
+        Me.TreeViewAll.Size = New System.Drawing.Size(227, 433)
+        Me.TreeViewAll.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.TabControlDetail)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(591, 465)
+        Me.Panel1.TabIndex = 0
+        '
+        'TabControlDetail
+        '
+        Me.TabControlDetail.Controls.Add(Me.TabPage3)
+        Me.TabControlDetail.Controls.Add(Me.TabPage4)
+        Me.TabControlDetail.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControlDetail.Location = New System.Drawing.Point(0, 0)
+        Me.TabControlDetail.Name = "TabControlDetail"
+        Me.TabControlDetail.SelectedIndex = 0
+        Me.TabControlDetail.Size = New System.Drawing.Size(591, 465)
+        Me.TabControlDetail.TabIndex = 0
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(583, 439)
+        Me.TabPage3.TabIndex = 0
+        Me.TabPage3.Text = "TabPage3"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'TabPage4
+        '
+        Me.TabPage4.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage4.Size = New System.Drawing.Size(249, 94)
+        Me.TabPage4.TabIndex = 1
+        Me.TabPage4.Text = "TabPage4"
+        Me.TabPage4.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(836, 512)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.TabControlList.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.TabControlDetail.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents 文件ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents msg As ToolStripStatusLabel
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents TabControlList As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TreeViewMe As TreeView
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TreeViewAll As TreeView
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents TabControlDetail As TabControl
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents TabPage4 As TabPage
+End Class
---
+++ ApplicationFrameworkLoader/frmMain.vb
@@ -0,0 +1,150 @@
+﻿Imports System.Reflection
+Imports System.IO
+Public Class frmMain
+    Const CLOSE_SIZE As Integer = 12
+    Public Sub New()
+
+        ' 此调用是设计器所必需的。
+        InitializeComponent()
+
+        ' 在 InitializeComponent() 调用之后添加任何初始化。
+        For Each mTabPage As TabPage In TabControlList.TabPages
+            For Each ctl As Control In mTabPage.Controls
+                If TypeOf ctl Is TreeView Then
+                    Dim mTreeView As TreeView = CType(ctl, TreeView)
+                    mTreeView.Nodes.Add(mTabPage.Text)
+
+                    mTreeView.ShowLines = True
+                    mTreeView.ShowPlusMinus = True
+                    mTreeView.ExpandAll()
+                End If
+            Next
+        Next
+        msg.Text = ""
+        TabControlDetail.TabPages.Clear()
+        TabControlDetail.DrawMode = TabDrawMode.OwnerDrawFixed
+        TabControlDetail.Padding = New System.Drawing.Point(CLOSE_SIZE, 0)
+        'TabControlDetail.DrawItem += New DrawItemEventHandler(this.tabControl2_DrawItem)
+        'TabControlDetail.MouseDown += New System.Windows.Forms.MouseEventHandler(this.tabControl2_MouseDown)
+        AddTabForm()
+    End Sub
+    Public Sub AddTabForm()
+        Dim paths() As String = Directory.GetFiles(Application.StartupPath & "\DllTabForms", "*.dll")
+        Dim RootNode As TreeNode = Me.TreeViewMe.Nodes(0)
+        For Each path As String In paths
+
+            Try
+                Dim DllAssembly As Assembly = Assembly.LoadFrom(path)
+                Dim DllTypes() As Type = DllAssembly.GetTypes()
+                For Each DllType As Type In DllTypes
+                    'Dim DllPropertyInfo As PropertyInfo = DllType.GetProperty("FormGUID")
+                    If Not DllType.GetInterface("IRecognize") Is Nothing Then
+                        Dim DllTabForm As Object = DllAssembly.CreateInstance(DllType.FullName)
+                        'Dim IsMainForm As Boolean = CallByName(DllTabForm, "IsMainForm", CallType.Get)
+                        Dim FormName As String = CallByName(DllTabForm, "FormName", CallType.Get)
+                        Dim ChildNode As TreeNode = RootNode.Nodes.Add(FormName)
+                        ChildNode.Tag = path & "|" & DllType.FullName
+
+                    End If
+                Next
+            Catch ex As Exception
+                Me.msg.Text = "错误信息：" & ex.Message
+            End Try
+        Next
+
+    End Sub
+
+    Private Sub TabControlDetail_DrawItem(sender As Object, e As DrawItemEventArgs)
+        Try
+            Dim myTabRect As Rectangle = TabControlDetail.GetTabRect(e.Index)
+            '先添加TabPage属性   
+            e.Graphics.DrawString(TabControlDetail.TabPages(e.Index).Text, Me.TabControlDetail.Font, SystemBrushes.ControlText, myTabRect.X + 2, myTabRect.Y + 2)
+
+            '再画一个矩形框
+            Using p As Pen = New Pen(Color.Black)
+                myTabRect.Offset(myTabRect.Width - (CLOSE_SIZE + 3), 2)
+                myTabRect.Width = CLOSE_SIZE
+                myTabRect.Height = CLOSE_SIZE
+                e.Graphics.DrawRectangle(p, myTabRect)
+            End Using
+
+            '填充矩形框
+            Dim recColor As Color = IIf(e.State = DrawItemState.Selected, Color.Orange, Color.DarkGray)
+            Using b As Brush = New SolidBrush(recColor)
+                e.Graphics.FillRectangle(b, myTabRect)
+            End Using
+            '画关闭符号
+            Using p As Pen = New Pen(Color.White)
+                '画"/"线
+                Dim p1 As Point = New Point(myTabRect.X + 3, myTabRect.Y + 3)
+                Dim p2 As Point = New Point(myTabRect.X + myTabRect.Width - 3, myTabRect.Y + myTabRect.Height - 3)
+                e.Graphics.DrawLine(p, p1, p2)
+
+                '画"/"线
+                Dim p3 As Point = New Point(myTabRect.X + 3, myTabRect.Y + myTabRect.Height - 3)
+                Dim p4 As Point = New Point(myTabRect.X + myTabRect.Width - 3, myTabRect.Y + 3)
+                e.Graphics.DrawLine(p, p3, p4)
+            End Using
+
+            e.Graphics.Dispose()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub TabControlDetail_MouseDown(sender As Object, e As MouseEventArgs)
+        If e.Button = MouseButtons.Left Then
+            Dim x As Integer = e.X
+            Dim y As Integer = e.Y
+
+            '计算关闭区域   
+            Dim myTabRect As Rectangle = TabControlDetail.GetTabRect(TabControlDetail.SelectedIndex)
+
+            myTabRect.Offset(myTabRect.Width - (CLOSE_SIZE + 3), 2)
+            myTabRect.Width = CLOSE_SIZE
+            myTabRect.Height = CLOSE_SIZE
+
+            '如果鼠标在区域内就关闭选项卡   
+            Dim isClose As Boolean = (x > myTabRect.X) And (x < myTabRect.Right) And (y > myTabRect.Y) And (y < myTabRect.Bottom)
+            If isClose Then TabControlDetail.TabPages.Remove(TabControlDetail.SelectedTab)
+        End If
+    End Sub
+
+    Private Sub TreeViewMe_AfterSelect(sender As Object, e As TreeViewEventArgs)
+
+    End Sub
+
+    Private Sub TreeViewMe_NodeMouseDoubleClick(sender As Object, e As TreeNodeMouseClickEventArgs)
+        Try
+            If TreeViewMe.SelectedNode.Level > 0 Then
+                For Each DetailPage As TabPage In Me.TabControlDetail.TabPages
+                    If DetailPage.Text = TreeViewMe.SelectedNode.Text Then
+                        Me.TabControlDetail.SelectedTab() = DetailPage
+                        Exit Sub
+                    End If
+                Next
+
+                Dim NodeInfo() As String = Split(TreeViewMe.SelectedNode.Tag, "|")
+                Dim DllAssembly As Assembly = Assembly.LoadFile(NodeInfo(0))
+                Dim DllTabForm As Object = DllAssembly.CreateInstance(NodeInfo(1))
+                Dim f As Form = CType(DllTabForm, Form)
+                f.FormBorderStyle = FormBorderStyle.None
+                f.AutoScroll = True
+                f.AutoScrollMinSize = New Size(TabControlDetail.Width * 0.8, TabControlDetail.Height * 0.8)
+                f.Dock = DockStyle.Fill
+                f.TopLevel = False
+                f.Show()
+                Dim mTab As New TabPage
+                mTab.Text = TreeViewMe.SelectedNode.Text
+                mTab.AutoScroll = True
+                mTab.Controls.Add(f)
+
+                Me.TabControlDetail.Font = New Font("微软雅黑", 10.5)
+                Me.TabControlDetail.TabPages.Add(mTab)
+                Me.TabControlDetail.SelectedTab() = mTab
+            End If
+        Catch ex As Exception
+            msg.Text = ex.Message
+        End Try
+    End Sub
+End Class
